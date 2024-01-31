cbe6b22423bb728dc6d4859c9485f45cf40a26cc
SarahKronsteiner <sarah.kronsteiner@iubh-fernstudium.de>
2024-01-31 12:29:03 +0100

    base structure portfolio website

--- portfolio-kronsteiner/src/app/app.component.ts
+++ portfolio-kronsteiner/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MyPortfolioComponent } from './my-portfolio/my-portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutMeComponent, ContactComponent, FooterComponent, HeaderComponent, MyPortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
