2ff5eaa1ba15a7c112bcf830d2198dff12769dcf
SarahKronsteiner <sarah.kronsteiner@iubh-fernstudium.de>
2024-03-07 14:02:37 +0100

    added detail header

---
+++ portfolio-kronsteiner/src/app/detail-header/detail-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-header.component.html',
+  styleUrl: './detail-header.component.css'
+})
+export class DetailHeaderComponent {
+
+}
