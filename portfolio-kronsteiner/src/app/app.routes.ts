c3fb0e3e81346f8c94e5bff04d8f5c5cfd06966b
SarahKronsteiner <sarah.kronsteiner@iubh-fernstudium.de>
2024-03-14 13:43:14 +0100

    removed angular

--- portfolio-kronsteiner/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { DetailPageComponent } from './detail-page/detail-page.component';
-import { HomeComponent } from './home/home.component';
-
-export
-const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'detail', component: DetailPageComponent }
-];
